50804e18943d0ec0de939dfc8735c13952e77108
Sushovan Paul <sushovan.paul@prismforce.ai>
2023-02-13 17:47:14 +0530

    added env to gitignore :

--- configs/db.js
+++ configs/db.js
@@ -2,5 +2,5 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-  uri: "postgres://anifsvjy:cLC1WSAlVhalh9fSs0WD1l4dkML28SA6@tai.db.elephantsql.com/anifsvjy",
+  uri: process.env.POSTGRESS_URI,
 };
